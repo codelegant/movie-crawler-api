d1231d8ab5c571a51b580e6174c6799d5578a286
codelegant <laichuanfeng@hotmail.com>
2016-11-04 21:13:12 +0800

    UPDATE 抓取数据并存储的 API

--- db/index.js
+++ db/index.js
@@ -19,18 +19,36 @@ module.exports = (()=>({
         if (res.result.ok !== 1) return cliLog.error('插入数据失败');
         cliLog.success(`成功插入${res.ops.length}条城市数据`);
         callback(res);
+        return res.ops.length;
       });
   },
-  findCities(db, callback){
+  /**
+   *
+   * @param db {Db}
+   * @param callback {Function}
+   * @param query {Object} 查询条件
+   * @return {Promise.<Array>}
+   */
+  findCities(db, callback, query = {}){
     return db
       .collection('cities')
-      .find({})
+      .find(query)
       .toArray()
       .then(docs=> {
         cliLog.success(`成功获取${docs.length}条城市数据`);
         callback(docs);
         return docs;
       });
+  },
+  /**
+   *
+   * @param db {Db}
+   * @param callback {Function}
+   * @param id {Number}
+   * @return {*|Promise.<Array>}
+   */
+  findCityById(db, callback, id){
+    return this.findCities(db, callback, {id});
   }
 }))();
 
--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const docOperate = require('./db/index');
 
 const server = restify.createServer();
 const url = 'mongodb://api:api@127.0.0.1:3000/movie?authMechanism=SCRAM-SHA-1';
+server.use(restify.queryParser());//使用 req.params，可以获取查询对象
 
 server.get('/taobao/cities', async(req, res)=>res.json(await taobao.getCityList()));
 server.get('/taobao/movies', async(req, res)=> {
@@ -51,36 +52,60 @@ server.get('/gewara/movies', async(req, res)=> {
   res.json(await gewara.getHotMovieList());
 });
 
-server.get('/cities', async(req, res)=> {
+
+server.get('/cities', async(req, res, next)=> {
   try {
     const docs = await MongoClient
       .connect(url)
       .then(db=> {
         return docOperate.findCities(db, ()=>db.close());
       });
-    const statusCode = docs.length ? 200 : 404;
-    res.json(statusCode, docs);
+    return docs.length ? res.json(200, docs) : next(new restify.NotFoundError('未查询到城市列表'));
   } catch (e) {
-    cliLog.error(e);
-    res.json(500, []);
+    return next(new restify.InternalServerError('获取城市列表失败'));
   }
 });
 
-server.get('/movies', async(req, res)=> {
+server.post('/cities', async(req, res, next)=> {
   try {
-    const movieLists = await Promise.all([taobao.getHotMovieList(), maoyan.getHotMovieList(), gewara.getHotMovieList()])
-      .then(movieLists=>movieLists);
-    let _movieList = _unionWith(movieLists[0], movieLists[1], movieLists[2], (src, target)=> {
-      if (src.name == target.name) return target.link = _merge(src.link, target.link);
+    const citiesObj = await cawler.citiesObj();
+    const citiesArr = [];
+    _mapKeys(citiesObj, (cities, key)=> {
+      for (const city of cities) {
+        city.initials = key;
+        citiesArr.push(city);
+      }
     });
-    _movieList = _remove(_movieList, movie=>movie.link.taobao);
-    res.json(_movieList);
-    console.log(_movieList.length);
+    const length = await MongoClient
+      .connect(url)
+      .then(db=> docOperate.insertCities(db, citiesArr, ()=>db.close()));
+    return res.send(length ? 201 : 204);
   } catch (e) {
-    console.log(e);
+    return next(new restify.InternalServerError('抓取或插入城市列表失败'));
   }
 });
 
+server.get('/movies', async(req, res, next)=> {
+  if (!req.params.cityId) return next(new restify.InvalidArgumentError('只收受 cityId 作为参数'));
+  res.json(req.params.cityId);
+  // try {
+  //   const movieLists = await Promise.all([taobao.getHotMovieList(), maoyan.getHotMovieList(), gewara.getHotMovieList()])
+  //     .then(movieLists=>movieLists);
+  //   let _movieList = _unionWith(movieLists[0], movieLists[1], movieLists[2], (src, target)=> {
+  //     if (src.name == target.name) return target.link = _merge(src.link, target.link);
+  //   });
+  //   _movieList = _remove(_movieList, movie=>movie.link.taobao);
+  //   res.json(_movieList);
+  //   console.log(_movieList.length);
+  // } catch (e) {
+  //   console.log(e);
+  // }
+});
+
+
+server.get('/movies/:id', async(req, res)=> {
+  res.json(req.params.id);
+});
 // (async()=> {
 //   const docs = await MongoClient
 //     .connect(url)

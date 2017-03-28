a3c124ea86d4fa0c0edc6651822c838acfed15c2
codelegant <laichuanfeng@hotmail.com>
2017-03-28 08:43:14 +0800

    UPDATED use native ES6 promises in Mongoose

--- db/connect.js
+++ db/connect.js
@@ -7,9 +7,10 @@
 const mongoose = require('mongoose');
 const cliLog = require('../libs/cliLog');
 const db_debug = require('debug')('db_debug');
+mongoose.Promise = global.Promise;
 // db.open('mongodb://api:api@127.0.0.1:1000/movie?authMechanism=SCRAM-SHA-1');
 
-module.exports = function connect () {
+module.exports = function connect() {
   global.g_connect = mongoose.connection;
   g_connect.on('error', err => cliLog.error(`Connection Error: ${err}`));
 
